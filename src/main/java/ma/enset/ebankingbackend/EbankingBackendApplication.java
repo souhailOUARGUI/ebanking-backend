12b86c9c394a2b60c97f1f30c267626c0f5b606e
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-18 13:08:35 +0100

    tested creating and fetching cutomers and BankAccount

--- src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,7 +1,11 @@
 package ma.enset.ebankingbackend;
 
 import ma.enset.ebankingbackend.dtos.AccountOperationDTO;
+import ma.enset.ebankingbackend.entities.BankAccount;
+import ma.enset.ebankingbackend.entities.CurrentAccount;
 import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.entities.SavingAccount;
+import ma.enset.ebankingbackend.enums.AccountStatus;
 import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
 import ma.enset.ebankingbackend.repositories.BankAccountRepository;
 import ma.enset.ebankingbackend.repositories.CustomerRepository;
@@ -10,6 +14,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -24,12 +30,49 @@ public class EbankingBackendApplication {
                             BankAccountRepository bankAccountRepository,
                             AccountOperationRepository accountOperationRepository){
         return args -> {
-          Stream.of("souhail","youssef","aicha").forEach(name -> {
-              Customer customer = new Customer();
-              customer.setName(name);
-              customer.setEmail(name+"@gmail.com");
-              customerRepository.save(customer);
-          });
+//          Stream.of("souhail","youssef","aicha").forEach(name -> {
+//              Customer customer = new Customer();
+//              customer.setName(name);
+//              customer.setEmail(name+"@gmail.com");
+//              customerRepository.save(customer);
+//          });
+//            customerRepository.findAll().forEach(cust -> {
+//                CurrentAccount currentAccount = new CurrentAccount();
+//                currentAccount.setId(UUID.randomUUID().toString());
+//                currentAccount.setCreatedAt(new Date());
+//                currentAccount.setBalance(Math.random()*90000);
+//                currentAccount.setOverdraft(1000);
+//                currentAccount.setStatus(AccountStatus.CREATED);
+//                currentAccount.setCurrency("USD");
+//                currentAccount.setCustomer(cust);
+//                bankAccountRepository.save(currentAccount);
+//
+//                SavingAccount savingAccount = new SavingAccount();
+//                savingAccount.setId(UUID.randomUUID().toString());
+//                savingAccount.setCreatedAt(new Date());
+//                savingAccount.setBalance(Math.random()*90000);
+//                savingAccount.setInterestRate(5.5);
+//                savingAccount.setStatus(AccountStatus.CREATED);
+//                savingAccount.setCurrency("USD");
+//                savingAccount.setCustomer(cust);
+//                bankAccountRepository.save(savingAccount);
+//
+//            });
+
+            BankAccount bankAccount = bankAccountRepository.findById("01c8eee9-7242-4439-acfd-1be354846ea9").orElse(null);
+            System.out.println("********************");
+            System.out.println(bankAccount.getId());
+            System.out.println(bankAccount.getCreatedAt());
+            System.out.println(bankAccount.getBalance());
+            System.out.println(bankAccount.getCurrency());
+            System.out.println(bankAccount.getStatus());
+            System.out.println(bankAccount.getCustomer().getName());
+            if (bankAccount instanceof CurrentAccount){
+               System.out.println("Over Draft=> "+ ((CurrentAccount) bankAccount).getOverdraft());
+            }else if (bankAccount instanceof SavingAccount){
+                System.out.println("Over Draft=> "+  ((SavingAccount) bankAccount).getInterestRate());
+            }
+
         };
     }
 
