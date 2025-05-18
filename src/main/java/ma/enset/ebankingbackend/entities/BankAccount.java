734678b96439d328348e9b54450c88949c234641
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-18 11:20:42 +0100

    Created main entities: Customer, AccountOperation and BankAccount

---
+++ src/main/java/ma/enset/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,29 @@
+package ma.enset.ebankingbackend.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.enset.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Inheritance( strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type",length = 4)
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private String currency;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation>  accountOperationsList;
+}
--- src/main/java/ma/enset/ebankingbackend/entities/Customer.java
+++ src/main/java/ma/enset/ebankingbackend/entities/Customer.java
@@ -1,16 +1,21 @@
 package ma.enset.ebankingbackend.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private List<BankAccount> bankAccounts;
+    @OneToMany(mappedBy = "customer")
+    private List<BankAccount> bankAccountsList;
 }
